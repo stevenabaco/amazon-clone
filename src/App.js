58df8ec43bc763a020256166a0cb37d2cdea88a2
Steven Abaco <stevenabaco@gmail.com>
2020-10-13 10:20:29 -0500

    Complete Header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from './Header';
+import './Header.css';
 
 function App() {
   return (
-
-    <Router>
-    <div className="app">
-      <Switch>
-          <Route path="/checkout">
-            <h1>Checkout !</h1>
-          </Route>
-          <Route path="/login">
-            <h1>Login Page !</h1>
-          </Route>
-          {/* This is the default route */}
-          <Route path="/">
-            <h1>Home Page !</h1>
-          </Route>
-      </Switch>
-    </div>
-    </Router>
-  );
+		<Router>
+			<div className='app'>
+				<Switch>
+					<Route path='/checkout'>
+						<h1>Checkout !</h1>
+					</Route>
+					<Route path='/login'>
+						<h1>Login Page !</h1>
+					</Route>
+					{/* This is the default route */}
+					<Route path='/'>
+						<Header />
+						<h1>Home Page !</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 {/* We NEED React-Router */ }
